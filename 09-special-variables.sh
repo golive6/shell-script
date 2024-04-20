f4b5fedcc56ecf465048b25ae05d38938ec3ebaf
golive6 <svravikiran.goli@gmail.com>
2024-04-20 06:30:12 -0400

    shell script practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "All variables:$@"
-echo "Number of variables passed:$#"
-echo "Script name:$0
-echo "Current working directory:$PWD"
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0
+echo "Current working directory: $PWD"
