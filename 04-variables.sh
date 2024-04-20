0239b4a0883b7bf881be1e018a4ba5c74056fad4
golive6 <svravikiran.goli@gmail.com>
2024-04-20 05:48:36 -0400

    shell script practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#Arguments/parameters:
+
 #Declaring of variables:
 
-PERSON1=Sachin
-PERSON2=Virat
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
 
 #referring a variables:
 echo "$PERSON1:: Hello $PERSON2, how are you doing?"
 echo "$PERSON2:: Hello $PERSON1, I am doing fine. How about you?"
 echo "$PERSON1:: I am fine too. How is your work?"
 echo "$PERSON2:: Work is going fine, thinking to upgrade my skills."
+echo "$PESRON3:: Hi $PERSON1 and $PERSON2, How are you guys doing?"
