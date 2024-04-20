aaf3b8f8cf5109a39bf3bffbd47dbe9562182aca
golive6 <svravikiran.goli@gmail.com>
2024-04-20 06:10:00 -0400

    shell script practice

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CITIES=("Toronto" "Mississauga" "Oakville" "Burlington")
+
+echo "1st City is: ${City[0]}"
+echo "2nd City is: ${City[1]}"
+echo "3rd City is: ${City[2]}"
+echo "All Cities are: ${Cities[@]}"
