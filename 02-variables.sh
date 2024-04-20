d736c61c32b7bd7f2f00c57ae1923c35e57005b2
golive6 <svravikiran.goli@gmail.com>
2024-04-20 05:25:30 -0400

    shell script practice

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, how are you doing?"
+echo "Suresh:: Hello Ramesh, I am doing fine. How about you?"
+echo "Ramesh:: I am fine too. How is your work?"
+echo "Suresh:: Work is going fine, thinking to upgrade my skills."
