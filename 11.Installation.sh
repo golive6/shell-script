0e0bd0f72f53e4ad4a4b1e9d122f1c3c691e99f9
golive6 <svravikiran.goli@gmail.com>
2024-04-20 07:34:34 -0400

    shell script practice

---
+++ 11.Installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u) #checking if the userid is root/super user or not.
+
+if [$USERID -ne 0]
+then 
+    echo "Please run this script with roor access."
+    exit 1 #manually exit if there is a error
+else
+    echo "You are a SUPER-USER."
+
+
+dnf intall mysqlllll -y
+
+if  [$? -ne 0]
+then 
+    echo "Installation of mysql....FAILURE"
+    exit 1
+else
+    echo "Installation of mysql....SUCCESS"
+fi
+
+
+dnf intall git -y
+
+if [$? -ne 0]
+then
+    echo "Installation of git....FAILURE"
+    exit 1
+else
+    echo "Installation of git....SUCCESS"
+fi
+
+echo "all installations are SUCCESSFUL :)"
