5efeec6f431b60c88714f07ebf470e37bcc4f684
golive6 <svravikiran.goli@gmail.com>
2024-04-20 06:15:44 -0400

    shell script practice

--- 07-array.sh
+++ 07-array.sh
@@ -4,5 +4,5 @@ CITIES=("Toronto" "Mississauga" "Oakville" "Burlington")
 
 echo "1st City is: ${CITIES[0]}"
 echo "2nd City is: ${CITIES[1]}"
-echo "City 1st and 4th are: ${CITIES[2,3]}"
+echo "City 1st and 4th are: ${CITIES[0],[3]}"
 echo "All Cities are: ${CITIES[@]}"
