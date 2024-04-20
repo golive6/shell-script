59bb6f6cec3825e54ecc5ec08d1e013e1bd3983d
golive6 <svravikiran.goli@gmail.com>
2024-04-20 06:32:07 -0400

    shell script practice

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -2,5 +2,8 @@
 
 echo "All variables: $@"
 echo "Number of variables passed: $#"
-echo "Script name: $0
+echo "Script Name: $0"
 echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
