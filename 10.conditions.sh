d31cfb5230ba84d1e2762d9a11c1752910ff1d90
golive6 <svravikiran.goli@gmail.com>
2024-04-20 07:01:16 -0400

    shell script practice

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -2,11 +2,11 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 40 ] 
+if [ $NUMBER -gt 10 ] 
 then
-    echo "Given number is greater than 40"
+    echo "Given number is greater than 10"
 else
-    echo "Given number is lesser than 40"
+    echo "Given number is lesser than 10"
 fi
 
 
