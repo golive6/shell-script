d822f79dffc045760bfec584b652b595e889c6b5
golive6 <svravikiran.goli@gmail.com>
2024-04-20 05:40:05 -0400

    shell script practice

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Declaring of variables:
+
+PERSON1=Ram
+PERSON2=Kevin
+
+echo "$PERSON1:: Hello $PERSON2, how are you doing?"
+echo "$PERSON2:: Hello $PERSON1, I am doing fine. How about you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: Work is going fine, thinking to upgrade my skills."
