fd09675a85974e70e92a890f399dea806f85a489
golive6 <svravikiran.goli@gmail.com>
2024-04-24 06:18:01 -0400

    shell script practice

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-for i in {1..20} &>>$LOGFILE #it will loop from 1 t0 20
+for i in {1..20} #it will loop from 1 t0 20
 do
   echo $i
 done
