5c8579305ad055830501de95f31756e42ef86ef8
golive6 <svravikiran.goli@gmail.com>
2024-04-26 14:48:00 -0400

    shell script practice

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# COURSE="DevOps from current script"
+
+# echo "Before calling otherscript, course: $COURSE"
+# echo "Process ID of current script: $$"
+
+# ./18-other-script.sh
+
+# echo "After calling otherscript, course: $COURSE"
+
+
+
 COURSE="DevOps from current script"
 
 echo "Before calling otherscript, course: $COURSE"
 echo "Process ID of current script: $$"
 
-./18-other-script.sh
+source ./18-other-script.sh
 
 echo "After calling otherscript, course: $COURSE"
