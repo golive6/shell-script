b7a08762273c90819970d7bc360ebd4aed230854
golive6 <svravikiran.goli@gmail.com>
2024-05-01 06:47:00 -0400

    shell script practice

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=6
 
 while IFS= read -r line
 do
-  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
   FOLDER=$(echo $line | awk -F " " '{print $NF}')
   if [ $USAGE -ge $DISK_THRESHOLD ] 
   then
